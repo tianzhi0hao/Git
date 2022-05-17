abf60d9f699d071bc0bf56f39813587d65a0618c
tianzhi0hao <105694378+tianzhi0hao@users.noreply.github.com>
2022-05-17 15:10:51 +0800

    pull commit

--- src/main/java/com/sharing/test/GitTest.java
+++ src/main/java/com/sharing/test/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello master fix1");
         System.out.println("Hello Git-hotfix2");
         System.out.println("push Git-hotfix2");
+        System.out.println("pull Git-hotfix2");
     }
 }
