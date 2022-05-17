6cd6fdc44e199bf33a4760bc48824c2593ea64b3
xuxiaolin <xuxiaolin94@qq.com>
2022-05-17 14:47:18 +0800

    second commit

--- src/main/java/com/sharing/test/GitTest.java
+++ src/main/java/com/sharing/test/GitTest.java
@@ -3,5 +3,6 @@ package com.sharing.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 }
